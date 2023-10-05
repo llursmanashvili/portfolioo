60744f6586d3fe8963c809ca101c6309e4f7cb02
llursmanashvili <lukacho.lursmanashvili+t2@gmail.com>
2023-10-05 17:51:24 +0400

    Updates

---
+++ precache-manifest.ccf56992257778b5ec6cda83d65ba788.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c066c5e3f40fc3361ba4a7638259de8",
+    "url": "/portfolioo/index.html"
+  },
+  {
+    "revision": "1b50aca5f3178dd7a0c4",
+    "url": "/portfolioo/static/css/main.1a11d210.chunk.css"
+  },
+  {
+    "revision": "b85aa40b54ee2788785e",
+    "url": "/portfolioo/static/js/2.8f7c2c30.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/portfolioo/static/js/2.8f7c2c30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b50aca5f3178dd7a0c4",
+    "url": "/portfolioo/static/js/main.b45b75da.chunk.js"
+  },
+  {
+    "revision": "4f62737692b606e9ac18",
+    "url": "/portfolioo/static/js/runtime-main.6d0aa155.js"
+  },
+  {
+    "revision": "9388d6146b3f9ee4678dfadeddc22c51",
+    "url": "/portfolioo/static/media/8secondz.9388d614.png"
+  },
+  {
+    "revision": "c7b2dbe09bb2846e7a55189bf9e51233",
+    "url": "/portfolioo/static/media/bartini.c7b2dbe0.png"
+  },
+  {
+    "revision": "7feddacba0a519c07a32a37f3f27c9c9",
+    "url": "/portfolioo/static/media/cabbagetreelandscape.7feddacb.png"
+  },
+  {
+    "revision": "ad8ee816190aba472e8bb754fc080d11",
+    "url": "/portfolioo/static/media/cactus.ad8ee816.png"
+  },
+  {
+    "revision": "9ef3cb7262237d548954ce357584c36f",
+    "url": "/portfolioo/static/media/cactuscan.9ef3cb72.png"
+  },
+  {
+    "revision": "ee0fdcd30f9f452e9d9af13840317663",
+    "url": "/portfolioo/static/media/matt.ee0fdcd3.png"
+  },
+  {
+    "revision": "9c1952ca5232cf092e46425369fa3111",
+    "url": "/portfolioo/static/media/psykinetic.9c1952ca.png"
+  }
+]);
